a6fce680b9c8344f7280966a24daf3954f0a9e28
권순규 <mouthgaebal@gmail.com>
2026-03-24 17:44:07 +0900

    PR 테스트용

---
+++ src/test/java/TestTestTest.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class TestTestTest {
+
+    @Test
+    void test() throws Exception {
+        assertThat(1 + 1).isEqualTo(2);
+    }
+
+}
