ec856cfb138487270737fe4562a397900b7c09c7
권시연 <yeon@woowahan.com>
2026-03-30 23:50:39 +0900

    feat: WhereCondition 내부에 condition 1개로 변경

--- src/main/java/builder/where/WhereCondition.java
+++ src/main/java/builder/where/WhereCondition.java
@@ -1,60 +1,39 @@
 package builder.where;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class WhereCondition {
 
-    private List<Condition> conditions;
-
-    private WhereCondition(List<Condition> conditions) {
-        this.conditions = conditions;
-    }
+    private Condition condition;
 
     public static WhereCondition empty() {
-        return new WhereCondition(new ArrayList<>());
+        return new WhereCondition();
     }
 
     public WhereCondition where(ComparisonCondition condition) {
-        initConditions();
-        conditions.add(condition);
+        this.condition = condition;
         return this;
     }
 
-    private void initConditions() {
-        if (conditions == null) {
-            conditions = new ArrayList<>();
-        }
-    }
-
     public WhereCondition and(ComparisonCondition condition) {
-        if (conditions == null || conditions.isEmpty()) {
+        if (this.condition == null) {
             throw new IllegalStateException("AND 조건을 추가하기 전에 먼저 WHERE 조건을 지정해주세요.");
         }
-        int lastIndex = conditions.size() - 1;
-        conditions.add(new LogicalCondition(conditions.get(lastIndex), condition, LogicalOperator.AND));
-        conditions.remove(lastIndex);
+        this.condition = new LogicalCondition(this.condition, condition, LogicalOperator.AND);
         return this;
     }
 
     public WhereCondition or(ComparisonCondition condition) {
-        if (conditions == null || conditions.isEmpty()) {
+        if (this.condition == null) {
             throw new IllegalStateException("OR 조건을 추가하기 전에 먼저 WHERE 조건을 지정해주세요.");
         }
-        int lastIndex = conditions.size() - 1;
-        conditions.add(new LogicalCondition(conditions.get(lastIndex), condition, LogicalOperator.OR));
-        conditions.remove(lastIndex);
+        this.condition = new LogicalCondition(this.condition, condition, LogicalOperator.OR);
         return this;
     }
 
     public String toSql() {
-        if (conditions.isEmpty()) {
+        if (condition == null) {
             return "";
         }
 
-        List<String> whereConditionStrings = conditions.stream()
-            .map(Condition::generateSqlString)
-            .toList();
-        return "WHERE " + String.join(" ", whereConditionStrings);
+        return "WHERE " + String.join(" ", condition.generateSqlString());
     }
 }
